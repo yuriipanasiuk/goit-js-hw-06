4233f1ce0742bb2a78fe11d89a97991e95e13eb5
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2022-07-20 21:27:06 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const ingradient = ingredients.map((element) => {
+  const createLi = document.createElement("li");
+  createLi.textContent = element;
+  createLi.classList.add("item");
+  list.append(createLi);
+});
