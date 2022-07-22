228366fb53f8037b8dbdbdf45f93f7adef58de1a
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2022-07-22 22:42:02 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formData = {
+    mail,
+    password,
+  };
+
+  mail === "" || password === ""
+    ? alert("усі поля повинні бути заповнені!!!")
+    : console.log(formData);
+
+  formRef.reset();
+}
